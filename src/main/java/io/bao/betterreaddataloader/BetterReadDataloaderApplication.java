b2a7409907fd38ca57194cef207f1000e6e310e9
Bao <thebaoking@gmail.com>
2022-08-28 12:28:20 -0700

    Initial Commit

---
+++ src/main/java/io/bao/betterreaddataloader/BetterReadDataloaderApplication.java
@@ -0,0 +1,13 @@
+package io.bao.betterreaddataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadDataloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterReadDataloaderApplication.class , args);
+    }
+
+}
---
+++ src/test/java/io/bao/betterreaddataloader/BetterReadDataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bao.betterreaddataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadDataloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
