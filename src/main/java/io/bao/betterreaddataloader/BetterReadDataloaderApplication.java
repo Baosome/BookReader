ecae932523de3db979a3d151bc609bdf66f78406
Bao <thebaoking@gmail.com>
2022-09-04 10:16:33 -0700

    Autowired and postconstruct in main

--- src/main/java/io/bao/betterreaddataloader/BetterReadDataloaderApplication.java
+++ src/main/java/io/bao/betterreaddataloader/BetterReadDataloaderApplication.java
@@ -1,6 +1,9 @@
 package io.bao.betterreaddataloader;
 
 import connection.DataStaxAstraProperties;
+import io.bao.betterreaddataloader.author.Author;
+import io.bao.betterreaddataloader.author.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +11,29 @@ import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomi
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadDataloaderApplication {
 
+    @Autowired AuthorRepository authorRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(BetterReadDataloaderApplication.class , args);
     }
+
+    @PostConstruct
+    public void start() {
+        Author author = new Author();
+        author.setId("ID");
+        author.setName("NAME");
+        author.setPersonalName("PERSONAL_NAME");
+        authorRepository.save(author);
+    }
+
+
 
     @Bean
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer (DataStaxAstraProperties astraProperties) {
--- src/main/java/io/bao/betterreaddataloader/author/AuthorRepository.java
+++ src/main/java/io/bao/betterreaddataloader/author/AuthorRepository.java
@@ -1,7 +1,9 @@
 package io.bao.betterreaddataloader.author;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends CassandraRepository<Author, String> {
 
 
