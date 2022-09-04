157c7c8645efdd926f111839e8c451d369c27344
Bao <thebaoking@gmail.com>
2022-09-04 10:07:03 -0700

    New author and author repository class

---
+++ src/main/java/io/bao/betterreaddataloader/author/AuthorRepository.java
@@ -0,0 +1,8 @@
+package io.bao.betterreaddataloader.author;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+public interface AuthorRepository extends CassandraRepository<Author, String> {
+
+
+}
